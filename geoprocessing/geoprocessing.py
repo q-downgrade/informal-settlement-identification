f74c4e31743c1a41d6c4f4123896cb38147d785f
danielmartinsheehan <daniel.martin.sheehan@gmail.com>
2020-02-12 08:31:36 -0500

    adding

--- geoprocessing/geoprocessing.py
+++ geoprocessing/geoprocessing.py
@@ -55,3 +55,17 @@ def get_fc_fields(fc):
     for field in fields:
         field_names.append(field.name)
     return field_names
+
+
+def intersect(in_fc_1, in_fc_2, output_fc, attributes="ALL"):
+    arcpy.Intersect_analysis(
+        "{} #;{} #".format(in_fc_1, in_fc_2),
+        out_feature_class=output_fc,
+        join_attributes=attributes,
+        cluster_tolerance="-1 Unknown",
+        output_type="INPUT",
+    )
+
+
+def dissolve_on_fields(input_fc, output_fc, dissolve_fields):
+    arcpy.Dissolve_management(input_fc, output_fc, dissolve_fields)
--- geoprocessing/geoprocessing_pipeline.py
+++ geoprocessing/geoprocessing_pipeline.py
@@ -5,22 +5,41 @@ from geoprocessing import *
 wd = 'Z:/Dropbox/General_Assembly/Projects/project_5/data'
 
 for g in data.keys():
+
+    # define study area extent for Sao Paolo/ Rio
+
     if g == 'rj':
         pass
     else:
+        print(f"running for {g}")
         geog = f"{wd}/{data[g]['census_tract']}"
         target = f"{wd}/{data[g]['favela']}"
-        # define study area extent for Sao Paolo/ Rio
 
-        fc_to_fc(geog, f"{wd}/processing/geog/", f"{g}_geog.shp")
+        for i in [[geog, 'geog'], [target, 'target']]:
+            fc_to_fc(
+                i[0],
+                f"{wd}/processing/processing.gdb/",
+                f"{g}_{i[1]}",
+            )
+
+            calculate_area(
+                f"{wd}/processing/processing.gdb/{g}_{i[1]}",
+                f"{i[1]}_orig_area",
+                "squaremeters",
+            )  # create orig area for census tract (aka geog) & favela (aka tar)
+
+        intersect(
+            f"{wd}/processing/processing.gdb/{g}_geog",
+            f"{wd}/processing/processing.gdb/{g}_target",
+            f"{wd}/processing/processing.gdb/{g}_geog_int_target",
+        )  # intersect geog and target
 
         calculate_area(
-            f"{wd}/processing/geog/{g}_geog.shp",
-            "orig_area",
+            f"{wd}/processing/processing.gdb/{g}_geog_int_target",
+            f"new_int_area",
             "squaremeters",
-        )  # create orig area for census tract (aka geog)
+        )
 
-        # create orig area for favela  (aka target)
         # create pct favela area for census tract (aka geog)
         # create favela binary flag for census tract (aka geog)
         # create population estimate for census tract (aka geog)
